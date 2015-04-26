89e650b05e2dedd31e17200bbbaf6e6bb5257406
jignesh2290 <parmarjignesh90@gmail.com>
2015-04-26 15:26:04 +0530

    Added About Us section

--- Mohindra/Mohindra/Content/js/mohindra.js
+++ Mohindra/Mohindra/Content/js/mohindra.js
@@ -19,7 +19,7 @@ $(document).ready(function () {
                 target = target.length ? target : $('[name=' + this.hash.slice(1) + ']');
                 if (target.length) {
                     $('html,body').animate({
-                        scrollTop: target.offset().top
+                        scrollTop: target.offset().top - 105
                     }, 1000);
                     return false;
                 }
