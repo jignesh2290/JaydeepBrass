3730911e5ddfdf3b5053096fced0603020eba6bd
jignesh2290 <parmarjignesh90@gmail.com>
2015-04-26 13:51:03 +0530

    Added Slider Navigation and home screen images

---
+++ Mohindra/Mohindra/Content/js/mohindra.js
@@ -0,0 +1,30 @@
+﻿/// <reference path="js/jquery-1.11.1.min.js" />
+$(document).ready(function () { 
+    
+    $(".scroll-link").click(function(e){
+    });
+
+    $(function () {
+        $('a[href*=#]:not([href=#])').click(function () {
+            if (location.pathname.replace(/^\//, '') == this.pathname.replace(/^\//, '') && location.hostname == this.hostname) {
+
+
+                $('.scroll-link').each(function () {
+                    $(this).removeClass('active');
+                });
+
+                $(this).parent('li').addClass('active');
+
+                var target = $(this.hash);
+                target = target.length ? target : $('[name=' + this.hash.slice(1) + ']');
+                if (target.length) {
+                    $('html,body').animate({
+                        scrollTop: target.offset().top
+                    }, 1000);
+                    return false;
+                }
+            }
+        });
+    });
+
+});
---
+++ Mohindra/Mohindra/Models/ContactViewModel.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+
+namespace Mohindra.Models
+{
+    public class ContactViewModel
+    {
+        [Required]
+        [Display(Name = "Name:")]
+        public string Name { get; set; }
+
+        [Required]
+        [EmailAddress]
+        [Display(Name = "Email:")]
+        public string Email { get; set; }
+
+        [Required]
+        [Display(Name = "Contact No:")]
+        public string ContactNo { get; set; }
+
+        [Display(Name = "Message:")]
+        public string Message { get; set; }
+    }  
+}
