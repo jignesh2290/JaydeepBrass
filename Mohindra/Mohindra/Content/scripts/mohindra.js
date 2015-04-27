8afe0e9bd94f8760392093d02ca62038a1a3f6a8
jignesh2290 <parmarjignesh90@gmail.com>
2015-04-28 01:06:17 +0530

    Fixed media queries for responsiveness

--- Mohindra/Mohindra/Content/scripts/mohindra.js
+++ Mohindra/Mohindra/Content/scripts/mohindra.js
@@ -1,40 +1,39 @@
 ﻿/// <reference path="js/jquery-1.11.1.min.js" />
 $(document).ready(function () {
     jQuery.easing.def = 'easeOutBack';
-    
-    $(".scroll-link").click(function(e){
-    });
 
-    $(function () {
-        $('a[href*=#]:not([href=#])').click(function () {
-            if (location.pathname.replace(/^\//, '') == this.pathname.replace(/^\//, '') && location.hostname == this.hostname) {
+    $('a[href*=#]:not([href=#])').click(function () {
+        if (location.pathname.replace(/^\//, '') == this.pathname.replace(/^\//, '') && location.hostname == this.hostname) {
 
+            //Toggle link selection
+            $('.scroll-link').each(function () {
+                $(this).removeClass('active');
+            });
 
-                $('.scroll-link').each(function () {
-                    $(this).removeClass('active');
-                });
+            $(this).parent('li').addClass('active');
 
-                $(this).parent('li').addClass('active');
+            //Hide the menu items on anchor click
+            $('.navbar-collapse').collapse('hide');
 
-                var scrollPadding = 0;
-                if ($(this).attr('href') == "#about") {
-                    scrollPadding = 125;
-                } else if ($(this).attr('href') == "#services") {
-                    scrollPadding = 78;
-                } else if ($(this).attr('href') == "#contactus") {
-                    scrollPadding = 125;
-                }
+            //$('.navbar-collapse').slideUp();
 
-                var target = $(this.hash);
-                target = target.length ? target : $('[name=' + this.hash.slice(1) + ']');
-                if (target.length) {
-                    $('html,body').animate({
-                        scrollTop: target.offset().top - scrollPadding
-                    }, 1300);
-                    return false;
-                }
+            var scrollPadding = 0;
+            if ($(this).attr('href') == "#about") {
+                scrollPadding = 125;
+            } else if ($(this).attr('href') == "#services") {
+                scrollPadding = 78;
+            } else if ($(this).attr('href') == "#contactus") {
+                scrollPadding = 125;
             }
-        });
-    });
 
+            var target = $(this.hash);
+            target = target.length ? target : $('[name=' + this.hash.slice(1) + ']');
+            if (target.length) {
+                $('html,body').animate({
+                    scrollTop: target.offset().top - scrollPadding
+                }, 1300);
+                return false;
+            }
+        }
+    });
 });
