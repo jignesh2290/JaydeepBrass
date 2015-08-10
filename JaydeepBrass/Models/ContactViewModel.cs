8c163ab9c0fd30078dd43f98bc780130cd406559
jignesh2290 <parmarjignesh90@gmail.com>
2015-08-10 17:54:07 +0530

    Initial Commit for the project

---
+++ JaydeepBrass/Models/ContactViewModel.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+
+namespace JaydeepBrass.Models
+{
+    public class ContactViewModel
+    {
+        [Required]
+        [Display(Name = "Name")]
+        public string Name { get; set; }
+
+        [Required]
+        [EmailAddress]
+        [Display(Name = "Email")]
+        public string Email { get; set; }
+
+        [Display(Name = "Contact")]
+        public string ContactNo { get; set; }
+
+        [Display(Name = "Message")]
+        public string Message { get; set; }
+    }  
+}
